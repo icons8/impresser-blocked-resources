3f35a91b599672df62b7c759663fe809205d6ad6
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-14 09:05:52 +0300

    Add defaults

--- test/main.js
+++ test/main.js
@@ -13,4 +13,12 @@ describe('impress-blocked-resource', function () {
     should(manager.resources.length).equal(2);
     should(manager.resources[1]).equal('resource-2');
   });
+
+  it('should defaults work', function() {
+    var
+      manager = new BlockedResource({ defaults: true });
+
+    should(manager.resources.length).greaterThan(0);
+    should(manager.resources).containDeep(['fonts.googleapis.com']);
+  });
 });
