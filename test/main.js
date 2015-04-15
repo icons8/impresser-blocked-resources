b5ca1f9b0a5f09b5f42bbf1feab2516585203f49
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-15 14:44:47 +0300

    Add optional flag

--- test/main.js
+++ test/main.js
@@ -8,7 +8,7 @@ var
 describe('impress-blocked-resource', function () {
   it('should work', function() {
     var
-      manager = new BlockedResource(configPath);
+      manager = new BlockedResource({ defaults: false, config: configPath });
 
     should(manager.resources.length).equal(2);
     should(manager.resources[1]).equal('resource-2');
@@ -16,9 +16,32 @@ describe('impress-blocked-resource', function () {
 
   it('should defaults work', function() {
     var
-      manager = new BlockedResource({ defaults: true });
+      manager = new BlockedResource();
 
     should(manager.resources.length).greaterThan(0);
     should(manager.resources).containDeep(['fonts.googleapis.com']);
+  });
+
+  it('should work optional flag', function(done) {
+    var
+      manager = new BlockedResource();
+
+    try {
+      manager.addConfig('file_not_exists', true);
+    }
+    catch(e) {
+      done(e);
+      return;
+    }
+
+    try {
+      manager.addConfig('file_not_exists');
+    }
+    catch(e) {
+      should(e).not.empty;
+      done();
+      return;
+    }
+    done('error');
   });
 });
